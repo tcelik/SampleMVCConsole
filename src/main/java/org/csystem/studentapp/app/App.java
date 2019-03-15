d557c1ca04ba054b429e63a9fe91518536042519
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-03-15 18:17:04 +0300

    StudentApp sınıfının run metodu ile çağrılması konsepti.

--- src/main/java/org/csystem/studentapp/app/App.java
+++ src/main/java/org/csystem/studentapp/app/App.java
@@ -3,6 +3,7 @@ package org.csystem.studentapp.app;
 public class App {
     public static void main(String[] args)
     {
-
+        var app = new StudentApp();
+        app.run();
     }
 }
---
+++ src/main/java/org/csystem/studentapp/app/StudentApp.java
@@ -0,0 +1,8 @@
+package org.csystem.studentapp.app;
+
+public class StudentApp {
+    public void run()
+    {
+
+    }
+}
